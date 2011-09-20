e55ec67392be51f6e42c84834f493a359f930c18
Gregory Jefferis <jefferis@gmail.com>
2011-09-20 05:20:19 +0100

    Factor out CollectSpikesFromSweeps from PlotRasterFromSweeps
    * can re-use spike time information for other purposes

--- RasterPlots.R
+++ RasterPlots.R
@@ -1,5 +1,7 @@
-#' Make a raster plot from a set of Nclamp sweeps recording odour responses  
-#' @param sweepdir directory containing Nclamp pxp sweep files 
+#' Make a raster plot from a set of Nclamp sweeps recording odour responses
+#'
+#' Note that can also give a spiketimes list from CollectSpikesFromSweeps
+#' @param sweepdir directory containing Nclamp sweep files (or list of spiketimes)
 #' @param sweeps Vector of sweeps to include (e.g. 1:7)
 #' @param xlim x axis range of plot 
 #' @param main main title of plot (see \code{\link{title}}) 
@@ -11,8 +13,10 @@
 #' @param odourRange time window of odour delivery 
 #' @param odourCol colour of odour window (default pale red) 
 #' @param relabelfun function to apply to odour labels (default no relabelling)
+#' @param IncludeChannels include numeric id of odour channel (e.g. for blanks)
 #' @return 
 #' @author jefferis
+#' @seealso CollectSpikesFromSweeps
 #' @export
 #' @example
 #' ## Plot time range 2-4s with odour pulse 2-3s for sweeps 0,1,3
@@ -26,10 +30,50 @@ PlotRasterFromSweeps<-function(sweepdir,sweeps,xlim=c(0,5000),
   main,sub,xlab='Time/ms', ylab='Odour',
   dotcolour='black',dotsize=0.5,
   odourRange=c(2000,2500),odourCol=rgb(1,0,0,alpha=.3),
-  relabelfun=identity,...){
+  relabelfun=identity,IncludeChannels=FALSE,...){
+  if(inherits(sweepdir,'spiketimes'))
+    rasterd=sweepdir
+  else
+    rasterd=CollectSpikesFromSweeps(sweepdir,sweeps)
+  last_wave=max(sapply(rasterd,function(x) max(x$Wave,na.rm=T)))
+  plot(NA,xlim=xlim,ylim=c(last_wave+1,0),ylab=ylab,xlab=xlab,axes=F,...)
+
+  labels=relabelfun(attr(rasterd,'oddconf')$odour)
+  if(IncludeChannels) labels=paste(labels,attr(rasterd,'oddconf')$chan)
+  axis(side=2,at=seq(last_wave+1)-0.5,labels=labels,tick=F,las=1)
+  axis(1)
+  nreps=length(rasterd)
+  for(i in seq(rasterd)){
+    yoff=i/(nreps+1)
+    df=rasterd[[i]]
+    points(x=df$Time,y=df$Wave+yoff,pch=22,col=NA,bg=dotcolour,cex=dotsize)
+  }
+  rect(odourRange[1],-0.5,odourRange[2],last_wave+1.5,col=odourCol,border=NA)
+  for(i in seq(last_wave+1)){
+    rect(0,i-1,5000,i,col=NA,border='black')
+  }
+  if(missing(main)) main=""
+  if(missing(sub)) sub=paste("Cell:",basename(attr(rasterd,'sweepdir')),
+    "sweeps:",paste(attr(rasterd,'sweeps'),collapse=","))
+  title(main=main,sub=sub)
+}
+
+#' Make a raster plot from a set of Nclamp sweeps recording odour responses
+#'
+#' The list of spiketimes has two columns, Time and Wave, where wave is 
+#' the number of the wave within each sweepfile containing the spike.
+#' @param sweepdir directory containing Nclamp pxp sweep files
+#' @param sweeps Vector of sweeps to include (e.g. 1:7)
+#' @return list (with class spiketimes) containing times for each sweep
+#' @author jefferis
+#' @export
+#' @example
+#' spikes=CollectSpikesFromSweeps("/Volumes/JData/JPeople/Jonny/physiology/data/nm20110811c0",c(0,1,3))
+#' PlotRasterFromSweeps(spikes,xlim=c(2000,4000),odourRange=c(2000,3000))
+CollectSpikesFromSweeps<-function(sweepdir,sweeps){
   # Read in all spike times
   ff=dir(sweepdir,'^[0-9]{3}_SP_',full=T)
-  rasterd=lapply(ff,read.table,col.names=c("Time","Sweep"),header=T, 
+  rasterd=lapply(ff,read.table,col.names=c("Time","Wave"),header=T, 
     na.strings='NAN')
   names(rasterd)=substring(basename(ff),1,3)
   
@@ -37,7 +81,10 @@ PlotRasterFromSweeps<-function(sweepdir,sweeps,xlim=c(0,5000),
     sweeps=names(rasterd)
   } else {
     sweeps=sprintf("%03d",sweeps)
-    rasterd=rasterd[sweeps]
+    if(all(sweeps%in%names(rasterd)))
+      rasterd=rasterd[sweeps]
+    else
+      stop("Missing spike counts for sweeps: ",setdiff(sweeps,names(rasterd)))
   }
 
   # read in ODD protocol
@@ -46,23 +93,14 @@ PlotRasterFromSweeps<-function(sweepdir,sweeps,xlim=c(0,5000),
     stringsAsFactors=FALSE)
   md5s=md5sum(oddfiles)
   if(length(unique(md5s))>1) stop("I dont yet know how to handle different odd config files")
+  
+  attr(rasterd,'oddconf')=oddconf
+  attr(rasterd,'sweeps')=sweeps
+  attr(rasterd,'sweepdir')=sweepdir
+  class(rasterd)=c('spiketimes',class(rasterd))
+  rasterd
+}
 
-  last_sweep=max(sapply(rasterd,function(x) max(x$Sweep,na.rm=T)))
-  plot(NA,xlim=xlim,ylim=c(last_sweep+1,0),ylab=ylab,xlab=xlab,axes=F,...)
-
-  axis(side=2,at=seq(last_sweep+1)-0.5,labels=relabelfun(oddconf$odour),tick=F,las=1)
-  axis(1)
-  nreps=length(rasterd)
-  for(i in seq(rasterd)){
-    yoff=i/(nreps+1)
-    df=rasterd[[i]]
-    points(x=df$Time,y=df$Sweep+yoff,pch=22,col=NA,bg=dotcolour,cex=dotsize)
   }
-  rect(odourRange[1],-0.5,odourRange[2],last_sweep+1.5,col=odourCol,border=NA)
-  for(i in seq(last_sweep+1)){
-    rect(0,i-1,5000,i,col=NA,border='black')
   }
-  if(missing(main)) main=""
-  if(missing(sub)) sub=paste("Cell:",basename(sweepdir),"sweeps:",paste(sweeps,collapse=","))  
-  title(main=main,sub=sub)
 }
