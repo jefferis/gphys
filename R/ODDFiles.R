d506457311846933b771b45806e54b0be89aa598
Gregory Jefferis <jefferis@gmail.com>
2012-01-28 06:28:16 +0000

    Cope with ODD files with missing zeros for delay/duration

--- R/ODDFiles.R
+++ R/ODDFiles.R
@@ -6,10 +6,13 @@
 #' Read in the ODD text file describing odour presentation sequence
 #' 
 #' @param oddfile Path to ODD file
+#' @param stringsAsFactors Whether to convert odours to strings (default) or factors
+#' @param fill Whether to fill in missing delay/duration values with NAs
+#' @param Verbose Show number of records read (default TRUE)
 #' @return dataframe containing a list of odours / presentation times
 #' @author jefferis
 #' @export
-read.odd<-function(oddfile){
+read.odd<-function(oddfile,fill=TRUE,stringsAsFactors=FALSE,Verbose=FALSE){
   ll=readLines(oddfile,warn=FALSE)
   tablelines=character(0)
   for(l in ll){
@@ -23,10 +26,13 @@ read.odd<-function(oddfile){
   }
   tc=textConnection(tablelines,'r')
   on.exit(close(tc))
-  res<-try(read.table(tc,col.names=make.unique(c('odour',rep(c('del','dur','chan'),5))),
-      stringsAsFactors=FALSE))
+  res<-try(scan(tc, quiet=!Verbose, fill=TRUE,
+          what=list('odour',0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+  
   if(inherits(res,'try-error')){
     stop("Malformed ODD file: ",oddfile)
   }
-  res
+  df=as.data.frame(res,stringsAsFactors=stringsAsFactors)
+  colnames(df)=make.unique(c('odour',rep(c('del','dur','chan'),5)))
+  df
 }
