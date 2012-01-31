297aa29d52e5c24dd07801958064659cd55482be
Gregory Jefferis <jefferis@gmail.com>
2012-01-31 02:06:38 +0000

    Add gphys.R to give a ?gphys help topic

---
+++ R/gphys.R
@@ -0,0 +1,8 @@
+#' Some simple functions for plotting electrophysiology data 
+#' collected in Igor Pro/Nclamp.
+#' gphys = Greg's ephys (and perhaps gee whiz) 
+#' @name gphys
+#' @docType package
+#' @title Analyse electrophysiology data acquired by Neuromatic / Igor Pro
+#' @keywords package
+NULL
