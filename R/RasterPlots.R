dd3618fc90e00496e5f661424456f149a4f95dc2
Gregory Jefferis <jefferis@gmail.com>
2012-03-09 11:09:43 +0000

    New split.spiketimes for experiments with repeats
    * assumes that each block consists of an identical number/order of odours

--- R/RasterPlots.R
+++ R/RasterPlots.R
@@ -187,3 +187,32 @@ PlotOdourResponseFromSpikes<-function(spiketimes,responseWindow,baselineWindow,
     PLOTFUN(bbdf,xlab='Spike Count',las=2,...)
   }
 }
+
+#' Split a spiketimes object with multiple repeats into list with one entry per repeat
+#' 
+#' Only works for spiketimes from single pxp file
+#' FIXME Teach this to cope with repeat blocks with different odours in different order
+#' @param st The spiketimes object
+#' @param blocksize number of waves per block - deduced from odour names if missing
+#' @return new spiketimes object (list) with one entry for each block of odours
+#' @author jefferis
+#' @export
+split.spiketimes<-function(st,blocksize){
+	if(length(st)>1) stop("Don't know how to split a spike time list of > length 1")
+	st1=st[[1]]
+	st1$OldWave=st1$Wave
+	odd=attr(st,'oddconf')
+	if(missing(blocksize))
+		blocksize=length(unique(odd$odour))
+	# Find 0 indexed repetition number
+	st1$Rep=floor(st1$OldWave/blocksize)
+	st1$Wave=st1$OldWave%%blocksize
+	
+	stnew=split(st1,st1$Rep)
+	mostattributes(stnew)=attributes(st)
+	if(!is.null(attr(stnew,'oddconf'))){
+		attr(stnew,'oddconf')=attr(stnew,'oddconf')[1:blocksize,]
+		
+	}
+	stnew
+}
