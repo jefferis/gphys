63900722e842034569240d10e60e8196778a85ad
Gregory Jefferis <jefferis@gmail.com>
2015-06-08 14:52:47 +0100

    test for lifetime sparseness with NAs

--- inst/tests/test-spikeanalysis.R
+++ inst/tests/test-spikeanalysis.R
@@ -8,4 +8,14 @@ test_that("lifetime_sparseness",{
                              baselineWindow = c(0,2000))
   expect_equal(lifetime_sparseness(od), 
                c(1, 1, 1, 0.977589526376588, 0.96039603960396))
+  
+  # check what happens with NAs
+  od2=od
+  # -ve response -> NA
+  od2[5,'cVA']=NA
+  # +ve response -> NA
+  od2[4,'IAA']=NA
+  # nb difference in baseline for cell 4 is due to loss of a positive response
+  expect_equal(lifetime_sparseness(od2),
+               c(1, 1, 1, 0.983362218370884, 0.96039603960396))
 })
