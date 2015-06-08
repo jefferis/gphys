a8d3e4540fd5bd4d982ee0b0c1f62490f1a7999d
Gregory Jefferis <jefferis@gmail.com>
2015-06-08 15:31:22 +0100

    move tests to new style testthat dir

--- tests/test-all.R
+++
@@ -1,8 +0,0 @@
-# Run all tests for gphys package
-#  
-# Author: jefferis
-###############################################################################
-
-library(testthat)
-library(gphys)
-test_package("gphys")
